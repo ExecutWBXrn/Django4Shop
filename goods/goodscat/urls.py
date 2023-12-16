4555f116c493247ef512fbbd7fd30769d2deb2cd
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-16 16:24:57 +0000

    replaced httpresponse on reverser + added templates

--- goods/goodscat/urls.py
+++ goods/goodscat/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,register_converter
 from .views import *
 from .converter import *
+
 register_converter(Sluglim5symbConverter,"Lim5Slug")
+
 urlpatterns = [
-    path('', index),
+    path('', index, name="home"),
+    path('aboutsite/', about, name="about"),
 ]
--- goods/goodscat/views.py
+++ goods/goodscat/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from django.template.loader import render_to_string
 
 def index(request):
-    return HttpResponse("goods")
+    return render(request, "goodscat/index.html")
 
+def about(request):
+    return render(request, "goodscat/about.html")
 def nfound(request, exception):
     return HttpResponseNotFound("not found page")
