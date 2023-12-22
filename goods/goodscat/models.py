696d42dac44df6401116f7525cadabfb6670b218
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-22 21:39:34 +0000

    add models

--- goods/goodscat/models.py
+++ goods/goodscat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Goods(models.Model):
+    good = models.CharField(max_length=255)
+    describe = models.TextField(blank=True)
+    price = models.IntegerField(default=None)
+    is_published = models.BooleanField(default=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
