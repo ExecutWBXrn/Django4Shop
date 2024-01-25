2eb0e2c01f7bcfbcde287f86b5826b6ebbe7deec
ExecutWBXrn <executwbxrn@gmail.com>
2024-01-25 21:13:27 +0000

    upgrade admin panel

--- goods/goodscat/admin.py
+++ goods/goodscat/admin.py
@@ -1,13 +1,25 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import *
 
 @admin.register(Goods)
 class GoodsAdmin(admin.ModelAdmin):
-    list_display = ["id", "good", "price", "time_create", "is_published", "cat"]
+    list_display = ["id", "good", "price", "time_create", "is_published", "cat", "brief_info"]
     list_display_links = ["id", "good"]
     list_per_page = 5
-    list_editable = "is_published",
-    ordering = "time_update", "good"
+    list_editable = ["is_published"]
+    ordering = ["time_update", "good"]
+    actions = ["set_published", "set_draft"]
+
+    @admin.display(description="further information", ordering="describe")
+    def brief_info(self, good: Goods):
+        return f"symbols in description: {len(good.describe)}"
+    @admin.action(description="published")
+    def set_published(self, request, queryset):
+        self.message_user(request, f"Кількість оновленних постів: {queryset.update(is_published=Goods.STATUS.PUBLISHED)}")
+
+    @admin.action(description="draft")
+    def set_draft(self, requset, queryset):
+        self.message_user(requset, f"{queryset.update(is_published=Goods.STATUS.DRAFT)} постів знято з публікації", messages.WARNING)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
